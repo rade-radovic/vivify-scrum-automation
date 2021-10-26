679b66447f3c0f68fa329e98b71d72172d1fcae5
rade-radovic <radovic.rade@gmail.com>
2021-10-26 16:45:20 +0200

    asserts FE

--- cypress/integration/boardCrud.spec.js
+++ cypress/integration/boardCrud.spec.js
@@ -2,23 +2,20 @@
 
 import login from '../fixtures/login-selectors.json'
 import data from "../fixtures/data.json"
-import board from "../fixtures/Board-selectors.json"
+import board from "../fixtures/board-selectors.json"
 
 describe('Board CRUD', () => {
 
-    it('visit vivify scrum', () => {
+    beforeEach('visit vivify scrum, login and go to organziation', () => {
         cy.visit("/", { timeout: 30000});
-    })
-
-    it('login', () => {
         cy.get(login.emailInput).clear().type(data.userBoardCrud.email)
         cy.get(login.passwordInput).clear().type(data.userBoardCrud.password)
         cy.get(login.loginButton).click()
+        cy.get(board.myOrganizationFooter, {timeout:30000}).click()
+        cy.get(board.modalOk).click()
     })
 
     it('add new board', () => {
-        cy.get(board.myOrganizationFooter, {timeout:30000}).click()
-        cy.get(board.modalOk).click()
         cy.get(board.addNewBoard).click()
         cy.get(board.boardNameInput).clear().type(data.board.name)
         cy.get(board.nextOrCreateButton).click()
@@ -26,34 +23,41 @@ describe('Board CRUD', () => {
         cy.get(board.nextOrCreateButton).click()
         cy.get(board.nextOrCreateButton).click()
         cy.get(board.nextOrCreateButton).click()
+        cy.get(board.boardTitle).should('have.text', data.board.name)
     })
 
     it('Create column', () => {
+        cy.get(board.boardHeader).eq(0).click()
         cy.get(board.addColumn).click()
         cy.get(board.column.nameInput).type(data.column.name + "{enter}")
+        cy.get(board.column.name).contains(data.column.name).should('have.text', data.column.name)
     })
 
     it('Add a task to the top of the column', () => {
+        cy.get(board.boardHeader).eq(0).click()
         cy.get(board.column.addTaskToTop).click({force:true})
         cy.get(board.task.nameInput).type(data.task.name + "{enter}")
+        cy.get(board.task.name).contains(data.task.name).should('have.text', data.task.name)
+
     })
 
     it('Add a task to the bottom of the column', () => {
+        cy.get(board.boardHeader).eq(0).click()
         cy.get(board.column.addTaskToBottom).eq(1).click({force:true})
         cy.get(board.task.nameInput).type(data.task.name2 + "{enter}")
+        cy.get(board.task.name).contains(data.task.name2).should('have.text', data.task.name2)
     })
 
     it('Cancel archive board', () => {
-        cy.get(board.myOrganizationFooter, {timeout:30000}).click()
-        cy.get(board.modalOk).click()
         cy.get(board.archiveBoard).click({force:true})
         cy.get(board.confirmArchiveNo).click()
+        cy.get(board.boardsSection).eq(0).children().should('have.length', 2)
+
     })
 
     it('Archive board', () => {
-        cy.get(board.myOrganizationFooter, {timeout:30000}).click()
-        cy.get(board.modalOk).click()
         cy.get(board.archiveBoard).click({force:true})
         cy.get(board.confirmArchiveYes).click()
+        cy.get(board.boardsSection).eq(0).children().should('have.length', 1)
     })
 })
--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -2,16 +2,10 @@
 
 const loginPage = require('../fixtures/login-selectors.json')
 import data from "../fixtures/data.json"
+import validationMessages from '../fixtures/validationMessages.json'
+import navigation from '../fixtures/navigation-selectors.json'
 
-describe('first cypress block', () => {
-
-    // it('firts test positive', () => {
-    //     expect(true).to.eq(true);
-    // })
-
-    // it('firts test negatives', () => {
-    //     expect(true).to.eq(false);
-    // })
+describe('Login', () => {
 
     it('visit vivify scrum', () => {
         cy.visit("/", { timeout: 30000});
@@ -19,44 +13,50 @@ describe('first cypress block', () => {
 
     it('all empty fields', () => {
         cy.get(loginPage.loginButton).click()
+        cy.get(loginPage.errorMessage).contains(validationMessages.invalidEmail).should('have.text', validationMessages.invalidEmail)
+        cy.get(loginPage.errorMessage).contains(validationMessages.passwordRequired).should('have.text', validationMessages.passwordRequired)
     })
 
     it('empty email field', () => {
         cy.get(loginPage.passwordInput).clear().type(data.user.password)
         cy.get(loginPage.loginButton).click()
+        cy.get(loginPage.errorMessage).contains(validationMessages.invalidEmail).should('have.text', validationMessages.invalidEmail)
     })
 
     it('empty password field', () => {
         cy.get(loginPage.emailInput).clear().type(data.user.email)
         cy.get(loginPage.passwordInput).clear()
         cy.get(loginPage.loginButton).click()
+        cy.get(loginPage.errorMessage).contains(validationMessages.passwordRequired).should('have.text', validationMessages.passwordRequired)
     })
 
     it('wrong password', () => {
         cy.get(loginPage.emailInput).clear().type(data.user.email)
         cy.get(loginPage.passwordInput).clear().type(data.negativeData.wrongPassword)
         cy.get(loginPage.loginButton).click()
+        cy.get(loginPage.backendErrorMessage).should('have.text', validationMessages.WrongEmailPassword)
     })
 
     it('wrong email', () => {
         cy.get(loginPage.emailInput).clear().type(data.user.email)
         cy.get(loginPage.passwordInput).clear().type(data.negativeData.wrongEmail)
         cy.get(loginPage.loginButton).click()
+        cy.get(loginPage.backendErrorMessage).should('have.text', validationMessages.WrongEmailPassword)
     })
 
     it('valid login', () => {
         cy.get(loginPage.emailInput).clear().type(data.user.email)
         cy.get(loginPage.passwordInput).clear().type(data.user.password)
         cy.get(loginPage.loginButton).click()
+        cy.get(navigation.scrumLogo).should('be.visible')
     })
 
     it('logout', () => {
         cy.get(loginPage.userButton, {timeout : 10000 }).click()
         cy.get(loginPage.profileButon).click()
         cy.get(loginPage.logoutButton).click()
+        cy.get(loginPage.loginButton).should('be.visible')
     })
-
-
 })
 
 
