4b8b0a8ac1c29f35b97a731ea69f713d0b8d9cea
rade-radovic <radovic.rade@gmail.com>
2021-10-30 19:08:11 +0200

    POM organization

--- cypress/models/navigationModule.js
+++ cypress/models/navigationModule.js
@@ -1,5 +1,8 @@
 module.exports = {
     get logoutButton() {
         return cy.get("button[class='vs-c-btn vs-c-btn--link vs-c-btn--danger']")
+    },
+    get scrumLogo() {
+        return cy.get("div[class='vs-c-site-logo vs-u-cursor--pointer']")
     }
 }
